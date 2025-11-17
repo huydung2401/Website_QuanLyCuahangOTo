f3c903b9b7c15d8b4ca3298c29d52cb486086948
huydung2401 <phamdung24012005@gmail.com>
2025-11-17 15:40:20 +0700

    thêm ảnh carouse05, 06, viết tài khoản controler đăng nhập và đăng kí.

--- TKW/Controllers/TaiKhoanController.cs
+++ TKW/Controllers/TaiKhoanController.cs
@@ -3,15 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TKW.Models;
 
 namespace TKW.Controllers
 {
     public class TaiKhoanController : Controller
     {
         // GET: TaiKhoan
-        public ActionResult Index()
+        QLMohoDBEntities2 db = new QLMohoDBEntities2();
+
+        // ========================
+        // GET: Đăng nhập
+        // ========================
+        public ActionResult DangNhap()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(string Email, string MatKhau)
+        {
+            var user = db.NguoiDungs
+                         .FirstOrDefault(x => x.Email == Email && x.MatKhau == MatKhau);
+
+            if (user == null)
+            {
+                TempData["Error"] = "Sai email hoặc mật khẩu!";
+                return RedirectToAction("DangNhap");
+            }
+
+            // Lưu session
+            Session["User"] = user;
+            Session["UserName"] = user.HoTen;
+            Session["UserId"] = user.IdNguoiDung;
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // ========================
+        // GET: Đăng ký
+        // ========================
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangKy(NguoiDung model)
+        {
+            // Check email tồn tại
+            if (db.NguoiDungs.Any(u => u.Email == model.Email))
+            {
+                TempData["Error"] = "Email này đã được sử dụng!";
+                return RedirectToAction("DangKy");
+            }
+
+            // Tạo mã ND tự động
+            model.IdNguoiDung = "ND" + (db.NguoiDungs.Count() + 1).ToString("00");
+            model.LaAdmin = false;
+
+
+            db.NguoiDungs.Add(model);
+            db.SaveChanges();
+
+            TempData["Success"] = "Đăng ký thành công! Hãy đăng nhập.";
+
+            return RedirectToAction("DangNhap");
+        }
+
+        // ========================
+        // Đăng xuất
+        // ========================
+        public ActionResult DangXuat()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
